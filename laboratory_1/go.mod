63c23e1de66c196fafbd46c4bbd8c0f6e26fa76f
Wilman Ortiz <willman.ortiz@gmail.com>
2021-09-26 15:29:08 -0500

    Define estructura y archivos bases del proyecto

---
+++ laboratory_1/go.mod
@@ -0,0 +1,5 @@
+module github.com/wortiz1027/laboratory_1
+
+go 1.17
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
---
+++ laboratory_1/internal/errors.go
@@ -0,0 +1,47 @@
+package internal
+
+import (
+	"fmt"
+)
+
+type ErrorCode uint
+
+type Error struct {
+	code    ErrorCode
+	message string
+	origin  error
+}
+
+const (
+	ErrorCodeUnknown ErrorCode = iota
+	ErrorCodeNotFound
+	ErrorCodeInvalidArgument
+)
+
+func WrapError(origin error, code ErrorCode, format string, a ...interface{}) error {
+	return &Error{
+		code:    code,
+		message: fmt.Sprintf(format, a...),
+		origin:  origin,
+	}
+}
+
+func NewError(code ErrorCode, format string, a ...interface{}) error {
+	return WrapError(nil, code, format, a...)
+}
+
+func (e *Error) Error() string {
+	if e.origin != nil {
+		return fmt.Sprintf("%s: %v", e.message, e.origin)
+	}
+
+	return e.message
+}
+
+func (e *Error) UnWrap() error {
+	return e.origin
+}
+
+func (e *Error) Code() ErrorCode {
+	return e.code
+}
